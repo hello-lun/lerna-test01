71aec3c866c05bd10629ac364347a17463e9eac9
kenjiding <kenjiding@tencent.com>
2021-11-05 23:37:41 +0800

    lerna 初始化

---
+++ packages/core/lib/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = core;
+
+function core() {
+  // TODO
+  console.log('我是core   lerna的packages');
+}
---
+++ packages/utils/lib/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = utils;
+
+function utils() {
+  // TODO
+  console.log('我是utils   lerna的packages');
+
+}
